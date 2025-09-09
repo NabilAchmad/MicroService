89f621def18001ea65594efec621aebd886b3206
NabilAchmad <nabilachmad316@gmail.com>
2025-09-09 13:04:09 +0700

    second:commit

--- orderservice/src/main/java/com/nabil/orderservice/OrderserviceApplication.java
+++ orderservice/src/main/java/com/nabil/orderservice/OrderserviceApplication.java
@@ -2,6 +2,8 @@ package com.nabil.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OrderserviceApplication {
@@ -10,4 +12,8 @@ public class OrderserviceApplication {
 		SpringApplication.run(OrderserviceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
